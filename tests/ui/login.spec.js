ca10d8aea66e6778ae7fa275528e02eef3dc3b78
Marriamer <mgalindocastillo6@gmail.com>
2026-04-01 16:34:49 -0500

    initial commit - ecommerce QA automation

---
+++ tests/ui/login.spec.js
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../../pages/LoginPage';
+
+test('Login exitoso', async ({ page }) => {
+  const login = new LoginPage(page);
+
+  await page.goto('https://www.saucedemo.com/');
+  await login.login('standard_user', 'secret_sauce');
+
+  await expect(page).toHaveURL(/inventory/);
+});
