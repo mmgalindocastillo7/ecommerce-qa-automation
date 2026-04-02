b5282e8378469d0e7f643e1d181051c3a0ba9e27
Marriamer <mgalindocastillo6@gmail.com>
2026-04-01 23:40:13 -0500

    increase timeout for CI stability

--- tests/ui/login.spec.js
+++ tests/ui/login.spec.js
@@ -1,10 +1,12 @@
 import { test, expect } from '@playwright/test';
-import { LoginPage } from '../../page/loginpage.js';
+import { LoginPage } from '../../pages/loginPage.js';
 
 test('Login exitoso', async ({ page }) => {
+  test.setTimeout(60000);
+
   const login = new LoginPage(page);
 
-  await page.goto('https://www.saucedemo.com/');
+  await page.goto('https://www.saucedemo.com/', { waitUntil: 'load' });
   await login.login('standard_user', 'secret_sauce');
 
   await expect(page).toHaveURL(/inventory/);
