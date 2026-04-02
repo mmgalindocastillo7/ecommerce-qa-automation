8b92a80c184979eb6f070883c1d65258d72a8f98
Marriamer <mgalindocastillo6@gmail.com>
2026-04-01 22:42:32 -0500

    fix import path for CI

--- tests/ui/login.spec.js
+++ tests/ui/login.spec.js
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-import { LoginPage } from '../../pages/LoginPage';
+import { LoginPage } from '../../page/loginpage.js';
 
 test('Login exitoso', async ({ page }) => {
   const login = new LoginPage(page);
